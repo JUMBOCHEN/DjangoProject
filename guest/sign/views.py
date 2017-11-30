daa6b10145af060cca577cd98188bfb2d1123ef4
JUMBOCHEN <641724352@qq.com>
2017-11-30 21:39:05 +0800

    还未编写模型

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == 'admin123':
+            response = HttpResponseRedirect('/event_manage/')
+            response.session['user'] = username
+            return response
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
+
+def event_manage(request):
+    username = request.session.get('user', '')
+    return render(request, 'event_manage.html', {'user':username})
+
