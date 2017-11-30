dd90c73aaa683fe942e3c4f3b37e77097f98e2a1
JUMBOCHEN <641724352@qq.com>
2017-11-30 22:16:52 +0800

    还未编写模型

--- guest/sign/models.py
+++ guest/sign/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#发布会表
+class Event(models.Model):
+    name = models.CharField(max_length=100)             #发布会标题
+    limit = models.IntegerField()                       #参加人数
+    stauts = models.BooleanField()                      #状态
+    address = models.CharField(max_length=200)          #地址
+    start_time = models.DateTimeField('event_time')     #发布会时间
+    create_time = models.DateTimeField(auto_now=True)   #创建时间
+        def __str__(self):
+            return self.name
