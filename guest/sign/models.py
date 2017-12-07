f0ea86ee5607b3c4660c71aea5f21f5ca2b42800
JUMBOCHEN <641724352@qq.com>
2017-12-07 22:49:50 +0800

    emmm...

--- guest/sign/models.py
+++ guest/sign/models.py
@@ -9,6 +9,7 @@ class Event(models.Model):
     address = models.CharField(max_length=200)          #地址
     start_time = models.DateTimeField('event_time')     #发布会时间
     create_time = models.DateTimeField(auto_now=True)   #创建时间
+
     def __str__(self):
         return self.name
 
@@ -18,7 +19,7 @@ class Guest(models.Model):
     realname = models.CharField(max_length=60)  #姓名
     phone = models.CharField(max_length=16)     #手机号
     email = models.EmailField()     #邮箱
-    realtime = models.BooleanField()        #签到状态
+    sign = models.BooleanField()        #签到状态
     create_time = models.DateTimeField(auto_now=True)   #创建时间（自动获取当前时间）
 
     class Meta:
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event, Guest
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -24,10 +26,10 @@ def login_action(request):
 def event_manage(request):
     event_list = Event.objects.all()
     username = request.session.get('user', '')
-    return render(request, 'event_manage.html', {'user': username, 'event': event_list})
+    return render(request, 'event_manage.html', {'user': username, 'events': event_list})
 
 def search_name(request):
-    username = request.session.get('username', '')
+    username = request.session.get('user', '')
     search_name = request.GET.get("name", "")
     search_name_bytes = search_name.encode(encoding="utf-8")
     event_list = Event.objects.filter(name__contains=search_name_bytes)
@@ -36,4 +38,29 @@ def search_name(request):
 def guest_manage(request):
     guest_list = Guest.objects.all()
     username = request.session.get('username', '')
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果页数不是整型, 取第一页.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果页数超出查询范围，取最后一页
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
+
+def search_phone(request):
+    username = request.session.get('user', '')
+    search_phone = request.GET.get("phone", "")
+    search_name_bytes = search_name.encode(encoding="utf-8")
+    guest_list = Guest.objects.filter(phone__contains=search_name_bytes)
     return render(request, "guest_manage.html", {"user": username, "guests": guest_list})
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    return response
+
+
