6b154ee57d0647e64b6733eeaaaaf468684ea9d1
JUMBOCHEN <641724352@qq.com>
2017-12-08 22:10:21 +0800

    emmm...

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     url(r'^search_name/$', views.search_name),
     url(r'^guest_manage/$', views.guest_manage),
     url(r'^logout/$', views.logout),
+    url(r'^sign_index/(?P<event_id>[0-9]+)/$', views.sign_index),
 ]
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
@@ -62,5 +62,10 @@ def logout(request):
     auth.logout(request)
     response = HttpResponseRedirect('/index/')
     return response
+
+@login_required
+def sign_index(request, event_id):
+    event = get_object_or_404(Event, event_id)
+    return render(request, 'sign_index.html', {'event': event})
 
 
