6fd179505bf4c4046513d848d372e8061a0856f1
JUMBOCHEN <641724352@qq.com>
2018-01-11 23:15:52 +0800

    2018.1.11

--- lakeface/mysite/article/views.py
+++ lakeface/mysite/article/views.py
@@ -11,13 +11,14 @@ from .forms import ArticleColumnForm, ArticlePostForm, ArticleTagForm
 
 import json
 
+
 @login_required(login_url='/account/login/')
-@csrf_exempt 
+@csrf_exempt
 def article_column(request):
     if request.method == "GET":
         columns = ArticleColumn.objects.filter(user=request.user)
         column_form = ArticleColumnForm()
-        return render(request, "article/column/article_column.html", {"columns":columns, 'column_form':column_form})
+        return render(request, "article/column/article_column.html", {"columns": columns, 'column_form': column_form})
 
     if request.method == "POST":
         column_name = request.POST['column']
@@ -28,6 +29,7 @@ def article_column(request):
             ArticleColumn.objects.create(user=request.user, column=column_name)
             return HttpResponse("1")
 
+
 @login_required(login_url='/account/login')
 @require_POST
 @csrf_exempt
@@ -35,8 +37,8 @@ def rename_article_column(request):
     column_name = request.POST["column_name"]
     column_id = request.POST['column_id']
     try:
-        line = ArticleColumn.objects.get(id=column_id)   
-        line.column = column_name   
+        line = ArticleColumn.objects.get(id=column_id)
+        line.column = column_name
         line.save()
         return HttpResponse("1")
     except:
@@ -50,15 +52,16 @@ def del_article_column(request):
     column_id = request.POST["column_id"]
     try:
         line = ArticleColumn.objects.get(id=column_id)
-        line.delete() 
+        line.delete()
         return HttpResponse("1")
     except:
         return HttpResponse("2")
 
+
 @login_required(login_url='/account/login')
 @csrf_exempt
 def article_post(request):
-    if request.method=="POST":
+    if request.method == "POST":
         article_post_form = ArticlePostForm(data=request.POST)
         if article_post_form.is_valid():
             cd = article_post_form.cleaned_data
@@ -70,10 +73,9 @@ def article_post(request):
                 tags = request.POST['tags']
                 if tags:
                     for atag in json.loads(tags):
-                        tag = request.user.tag.get(tag=atag) 
-                        new_article.article_tag.add(tag) 
+                        tag = request.user.tag.get(tag=atag)
+                        new_article.article_tag.add(tag)
 
-                
                 return HttpResponse("1")
             except:
                 return HttpResponse("2")
@@ -81,34 +83,37 @@ def article_post(request):
             return HttpResponse("3")
     else:
         article_post_form = ArticlePostForm()
-        article_columns = request.user.article_column.all() 
+        article_columns = request.user.article_column.all()
         article_tags = request.user.tag.all()
-        return render(request, "article/column/article_post.html", {"article_post_form":article_post_form, "article_columns":article_columns, "article_tags":article_tags})
+        return render(request, "article/column/article_post.html",
+                      {"article_post_form": article_post_form, "article_columns": article_columns,
+                       "article_tags": article_tags})
 
 
 @login_required(login_url='/account/login')
 def article_list(request):
-    #articles = ArticlePost.objects.filter(author=request.user) 
-    #return render(request, "article/column/article_list.html", {"articles":articles})
+    # articles = ArticlePost.objects.filter(author=request.user)
+    # return render(request, "article/column/article_list.html", {"articles":articles})
     articles_list = ArticlePost.objects.filter(author=request.user)
     paginator = Paginator(articles_list, 2)
-    page = request.GET.get('page')   
+    page = request.GET.get('page')
     try:
-        current_page = paginator.page(page)    
-        articles = current_page.object_list    
-    except PageNotAnInteger:    
+        current_page = paginator.page(page)
+        articles = current_page.object_list
+    except PageNotAnInteger:
         current_page = paginator.page(1)
         articles = current_page.object_list
-    except EmptyPage:    
-        current_page = paginator.page(paginator.num_pages)    
+    except EmptyPage:
+        current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
-    return render(request, "article/column/article_list.html", {"articles":articles, "page": current_page})
+    return render(request, "article/column/article_list.html", {"articles": articles, "page": current_page})
 
 
 @login_required(login_url='/account/login')
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id=id, slug=slug)
-    return render(request, "article/column/article_detail.html", {"article":article})
+    return render(request, "article/column/article_detail.html", {"article": article})
+
 
 @login_required(login_url='/account/login')
 @require_POST
@@ -120,7 +125,7 @@ def del_article(request):
         article.delete()
         return HttpResponse("1")
     except:
-    	return HttpResponse("2")
+        return HttpResponse("2")
 
 
 @login_required(login_url='/account/login')
@@ -129,9 +134,11 @@ def redit_article(request, article_id):
     if request.method == "GET":
         article_columns = request.user.article_column.all()
         article = ArticlePost.objects.get(id=article_id)
-        this_article_form = ArticlePostForm(initial={"title":article.title})
+        this_article_form = ArticlePostForm(initial={"title": article.title})
         this_article_column = article.column
-        return render(request, "article/column/redit_article.html", {"article":article, "article_columns":article_columns, "this_article_column":this_article_column, "this_article_form":this_article_form})
+        return render(request, "article/column/redit_article.html",
+                      {"article": article, "article_columns": article_columns,
+                       "this_article_column": this_article_column, "this_article_form": this_article_form})
     else:
         redit_article = ArticlePost.objects.get(id=article_id)
         try:
@@ -143,13 +150,15 @@ def redit_article(request, article_id):
         except:
             return HttpResponse("2")
 
+
 @login_required(login_url='/account/login')
 @csrf_exempt
 def article_tag(request):
     if request.method == "GET":
         article_tags = ArticleTag.objects.filter(author=request.user)
         article_tag_form = ArticleTagForm()
-        return render(request, "article/tag/tag_list.html", {"article_tags":article_tags, "article_tag_form":article_tag_form})
+        return render(request, "article/tag/tag_list.html",
+                      {"article_tags": article_tags, "article_tag_form": article_tag_form})
 
     if request.method == "POST":
         tag_post_form = ArticleTagForm(data=request.POST)
@@ -164,6 +173,7 @@ def article_tag(request):
         else:
             return HttpResponse("sorry, the form is not valid.")
 
+
 @login_required(login_url='/account/login')
 @require_POST
 @csrf_exempt
@@ -174,5 +184,4 @@ def del_article_tag(request):
         tag.delete()
         return HttpResponse("1")
     except:
-    	return HttpResponse("2")
-
+        return HttpResponse("2")
